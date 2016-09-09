8cd6f1011c799392e60b8653f6e35aa6c000e210
emerson <emerson@epplkraw0166t1.princeton.epam.com>
2016-09-09 10:21:00 -0500

    member 3 is intended to be used for backups, so decreasing it's priority

--- primary.js
+++ primary.js
@@ -2,6 +2,6 @@ cfg = rs.conf()
 cfg.members[0].priority = 0.5
 cfg.members[1].priority = 0.5
 cfg.members[2].priority = 1 
-cfg.members[3].priority = 0.5
+cfg.members[3].priority = 0.4
 cfg.members[4].priority = 0.5
 rs.reconfig(cfg)
