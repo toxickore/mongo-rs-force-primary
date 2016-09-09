9ff39f9fbe9a1ee88b7faedebd3bb3c4800633a3
emerson <emerson@epplkraw0166t1.princeton.epam.com>
2016-09-09 16:42:20 -0500

    fixed the comment blocks for log files

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$(sudo docker network ls | grep mongo_cluster | wc -l)" -eq "0" ];
 then
-	echo #################################
-	echo # creating docker mongo_cluster #
-	echo #################################
+	echo "#################################"
+	echo "# creating docker mongo_cluster #"
+	echo "#################################"
 	sudo docker network create mongo_cluster
 fi
 
@@ -12,18 +12,18 @@ sudo docker run -p 28002:27017 --name mongo_instance_002 --net mongo_cluster mon
 sudo docker run -p 28003:27017 --name mongo_instance_003 --net mongo_cluster mongo_ex mongod --setParameter authenticationMechanisms=MONGODB-CR --replSet rs0 &
 sudo docker run -p 28004:27017 --name mongo_instance_004 --net mongo_cluster mongo_ex mongod --setParameter authenticationMechanisms=MONGODB-CR --replSet rs0 &
 sudo docker run -p 28005:27017 --name mongo_instance_005 --net mongo_cluster mongo_ex mongod --setParameter authenticationMechanisms=MONGODB-CR --replSet rs0 &
-echo #################################
-echo #    Starting Replication Set   #
-echo #################################
+echo "#################################"
+echo "#    Starting Replication Set   #"
+echo "#################################"
 sleep 15
 mongo --port 28001 start_rs.js
-echo #################################
-echo #  Forcing node 02 as primary   #
-echo #################################
+echo "#################################"
+echo "#  Forcing node 02 as primary   #"
+echo "#################################"
 sleep 20
 mongo --port 28001 primary.js
-echo #################################
-echo #    Add arbiter member         #
-echo #################################
+echo "#################################"
+echo "#    Add arbiter member         #"
+echo "#################################"
 sleep 20
 mongo --port 28003 arbiter.js
